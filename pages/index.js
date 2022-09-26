6bacac55cd680fa8ec93c07678d935a13786fd32
ahmadrosid <alahmadrosid@gmail.com>
2022-09-26 12:42:11 +0700

    feat: update using DM Sans font

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,10 @@ export default function Home() {
         <title>Next.js and tailwindcss boilerplate!</title>
       </Head>
       <LinearGradient option={1} width="100vw" height="100vh">
-        <div className='text-center space-y-8'>
-          <h1 className='text-9xl text-white'>Boilerplate</h1>
-          <h2 className='text-6xl  bg-clip-text text-transparent bg-gradient-to-bl from-sky-300 to-blue-200'>Next.js + Tailwindcss</h2>
+        <div className='text-center space-y-8 font-sans'>
+          <h1 className='text-9xl text-white font-bold'>Boilerplate</h1>
+          <h2 className='text-6xl '>Next.js + Tailwindcss</h2>
+          <p>Build your next Amazing product!</p>
         </div>
       </LinearGradient>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['DM Sans', "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
